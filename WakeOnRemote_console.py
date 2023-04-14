e1f66050829502847e3f92540549f3c6209e3963
NHPT <42366434+NHPT@users.noreply.github.com>
2023-04-14 09:51:34 +0800

    Create WakeOnRemote_console.py

---
+++ WakeOnRemote_console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import sys
+import socket
+
+if len(sys.argv)<2:
+	print('Usage: Python3',sys.argv[0],'[IP Address] [MAC Address] [UDP Port,default=8]')
+	print('Example: Python3',sys.argv[0],'192.168.1.3 28-6E-D4-89-37-BA')
+	print('         Python3',sys.argv[0],'192.168.1.3 28-6E-D4-89-37-BA 9')
+elif len(sys.argv)<4:
+	Wake()
+else:
+	Wake(int(sys.argv[3]))
+
+def Wake(port=8):
+	mac="".join(sys.argv[2].split(':'))
+	mac="".join(sys.argv[2].split('-'))
+	data = b"\xFF\xFF\xFF\xFF\xFF\xFF" + bytes().fromhex(mac) * 16
+	s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	s.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
+	try:
+		s.sendto(data,(sys.argv[1],port))
+		print('[+] 已发送唤醒请求！')
+	except:
+		print('[!] 发送失败，请检查IP地址或域名！')
+	s.close()
